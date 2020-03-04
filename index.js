2a17f3195f796140de36662aec0236d001bc5416
sri-11 <bksubiksha@gmail.com>
2020-03-04 16:45:48 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 ///----------Task-4---//
 
-let st=prompt("enter your name","");
-  let st1=prompt("enter start slice","")
-let st2=prompt("enter end slice","")
- let fin=st.slice(st1,st2);
-alert(fin);
+// let st=prompt("enter your name","");
+//   let st1=prompt("enter start slice","")
+// let st2=prompt("enter end slice","")
+//  let fin=st.slice(st1,st2);
+// alert(fin);
 
